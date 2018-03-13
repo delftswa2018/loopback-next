22347cc0f0b1d316c9b5ea33953b9bcdb6c27e00
Yann Rosema <yann.rosema@hotmail.com>
2018-03-13 16:18:57 +0100

    refactor(examples): move application start error catch from main function to main index
    A similar refactoring as in the CLI generator template is applied to the examples to complete the
    fix for issue #925
    
    fix #925

--- packages/example-getting-started/src/index.ts
+++ packages/example-getting-started/src/index.ts
@@ -8,12 +8,8 @@ import {RestServer} from '@loopback/rest';
 
 export async function main() {
   const app = new TodoApplication();
-  try {
-    await app.boot();
-    await app.start();
-  } catch (err) {
-    console.error(`Unable to start application: ${err}`);
-  }
+  await app.boot();
+  await app.start();
   const server = await app.getServer(RestServer);
   console.log(`Server is running on port ${await server.get('rest.port')}`);
   return app;
